edbad2578816b4a47b1011628364681d691c0417
JiWon Lee <jiwon.swdev@gmail.com>
2020-10-16 19:56:46 +0900

    feat: rename project

---
+++ Hazzle/Hazzle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hazzle
+//
+//  Created by 이지원 on 2020/10/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
