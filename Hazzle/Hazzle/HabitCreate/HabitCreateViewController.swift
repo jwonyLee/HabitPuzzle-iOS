f64febae9da64b3f36f5193448fda9e76c2ebcd7
JiWon Lee <jiwon.swdev@gmail.com>
2020-10-30 17:39:59 +0900

    [#4] feat: 스크롤뷰 생성

--- Hazzle/Hazzle/HabitCreate/HabitCreateViewController.swift
+++ Hazzle/Hazzle/HabitCreate/HabitCreateViewController.swift
@@ -8,22 +8,30 @@
 import UIKit
 
 class HabitCreateViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - Properties
+    lazy var contentViewSize = CGSize(width: self.view.frame.width, height: self.view.frame.height + 400)
+    
+    // MARK: - Views
+    lazy var containerView = UIView().then {
+        $0.backgroundColor = .white
+        $0.frame.size = contentViewSize
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    lazy var scrollView = UIScrollView().then {
+        $0.frame = .zero
+        $0.backgroundColor = .white
+        $0.frame = self.view.bounds
+        $0.contentSize = contentViewSize
+        $0.autoresizingMask = .flexibleHeight
+        $0.showsHorizontalScrollIndicator = true
+        $0.bounces = true
     }
-    */
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(containerView)
+    }
 }
